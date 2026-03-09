5517b3b0ca7abe1881bdd9b2cefabde26025a7ce
Chris Haycock <chrishaycock@hotmail.com>
2026-03-09 09:26:54 +0100

    refactor(v2.0.0): rename _NPCScan to EbonSearch (plagiarism safe)
    - Folders: _NPCScan/ -> EbonSearch/, _NPCScanOverlay/ -> EbonOverlay/
    - Files: _NPCScan.lua -> EbonSearch.lua, _NPCScan.Overlay.lua -> EbonOverlay.lua, etc.
    - Global: _NPCScan -> EbonSearch, _NPCScanButton -> EbonSearchButton
    - SavedVars: _NPCScanOptions -> EbonSearchDB, _NPCScanOptionsCharacter -> EbonSearchCharacterDB
    - Overlay SavedVars: _NPCScanOverlayOptions -> EbonOverlayDB
    - Message bus: NpcOverlay_* -> EbonOverlay_*
    - Bindings: _NPCSCAN_BUTTONDRAG -> _EBONSEARCH_BUTTONDRAG
    - TOC: OptionalDeps EbonSearch, SavedVariables EbonSearchDB/EbonSearchCharacterDB
    - Intentionally kept: local 'me' alias (renaming would corrupt name/time/frame substrings)

---
+++ EbonSearch/Locales/Locale-zhCN.lua
@@ -0,0 +1,17 @@
+﻿--[[****************************************************************************
+  * EbonSearch by Saiket                                                         *
+  * Locales/Locale-zhCN.lua - Localized string constants (zh-CN).              *
+  ****************************************************************************]]
+
+
+if ( GetLocale() ~= "zhCN" ) then
+	return;
+end
+
+
+-- See http://wow.curseforge.com/addons/npcscan/localization/zhCN/
+local EbonSearch = select( 2, ... );
+EbonSearch.L.NPCs = setmetatable( {
+	[ 18684 ] = "独行者布罗加斯",
+	[ 33776 ] = "古德利亚",
+}, { __index = EbonSearch.L.NPCs; } );
